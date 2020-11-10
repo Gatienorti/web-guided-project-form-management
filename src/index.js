706310ed35e4c0b759c836c3630f111d72a1dbc5
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-11-10 11:32:12 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ function SimpleForm() {
   };
 
   const submit = (evt) => {
+    evt.preventDefault();
     const newPet = {
       petName: formValues.petName.trim(),
       petType: formValues.petType.trim(),

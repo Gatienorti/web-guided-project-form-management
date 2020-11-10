7e46223c9ddda79cbd98f009624e370899148154
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-11-10 12:13:52 -0600

    wip

--- src/components/FriendForm.js
+++ src/components/FriendForm.js
@@ -59,7 +59,8 @@ export default function FriendForm(props) {
           Role
           {/* 🔥 STEP 5 - Make dropdown for role. */}
           <select name="role" value={values.role} onChange={onChange}>
-            <value
+            <option value=""></option>
+            
           </select>
         </label>
 
